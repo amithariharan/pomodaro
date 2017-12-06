50f5f8d621e797bbd3d543ac0226021fa7cde8ae
amit <amit@qburst.com>
2017-12-06 17:40:18 +0530

    style changes

--- js/index.js
+++ js/index.js
@@ -32,11 +32,13 @@
       let startTime = time.value;
       setTimeout(playAlarm, startTime*60*1000)
       tick.play();
+      document.getElementsByClassName('material-icons')[2].innerHTML = 'pause';
       document.getElementsByClassName('reading-wrapper')[0].classList.add("animation-move");
     }
 
     playAlarm = () => {
-      console.log("sss");
+      document.getElementsByClassName('material-icons')[2].innerHTML = 'play_arrow';
+      document.getElementsByClassName('reading-wrapper')[0].classList.remove("animation-move");
       tick.pause();
       alarm.play()
     }
